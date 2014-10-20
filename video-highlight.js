8e3a269d36fe987da464df35585c4fb482a5c377
andychups <andychups@ya.ru>
2014-10-20 22:33:28 +0400

    Bloody refactoring

--- video-highlight.js
+++ video-highlight.js
@@ -1,67 +1,122 @@
-$.fn.videoHighlight = function (options) {
-    options = $.extend({
-        'timeUpdate': 300,
-        'debug': false
-    }, options);
-
-    var plugin = {};
+$.fn.videoHighlight = function () {
     var CANVAS_WIDTH = 1;
     var CANVAS_HEIGHT = 1;
-    var DEBUG = options.debug;
 
-    plugin.$video = this;
-    plugin.video = this[0];
-    plugin.$canvas = $('<canvas></canvas>');
-    plugin.canvas = plugin.$canvas[0];
-    plugin.ctx = plugin.canvas.getContext('2d');
-    plugin.getColorPoll = null;
+    /**
+     * @param {Object} options
+     * @param {Object} options.$video – jQuery object with video tag
+     * @constructor
+     */
+    function Plugin (options) {
+        this.CANVAS_WIDTH = 1;
+        this.CANVAS_HEIGHT = 1;
 
-    plugin.log = function () {
-        var args;
+        this.$video = options.$video;
+        this.video = this.$video[0];
+        this.$canvas = $('<canvas></canvas>');
+        this.canvas = this.$canvas[0];
+        this.ctx = this.canvas.getContext('2d');
+        this.getColorPoll = null;
+        this.timeUpdate = options.timeUpdate || 300;
 
-        if (!DEBUG) {
-            return;
+        this.canvas.width = this.CANVAS_WIDTH;
+        this.canvas.height = this.CANVAS_HEIGHT;
+        this._addEventListeners();
+    }
+
+    /**
+     * @private
+     * Grab to canvas for pixel array
+     * @returns {CanvasPixelArray}
+     */
+    Plugin.prototype._getMiddleColor = function () {
+        if (!this.video) {
+            clearInterval(this.getColorPoll);
+            this.getColorPoll = null;
         }
 
-        args = Array.prototype.slice.call(arguments);
-        args.unshift('videoHighlight >');
-
-        console.log.apply(console, args);
+        this.ctx.drawImage(this.video, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        return this.ctx.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT).data;
     };
 
-    plugin.getMiddleColor = function () {
-        var frameData;
+    /**
+     * @private
+     * Add event listeners
+     */
+    Plugin.prototype._addEventListeners = function () {
+        var _this = this;
 
-        if (!plugin.video) {
-            clearInterval(plugin.getColorPoll);
-            plugin.getColorPoll = null;
-        }
-
-        plugin.ctx.drawImage(plugin.video, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-        frameData = plugin.ctx.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT).data;
-
-        options.callback([frameData[0], frameData[1], frameData[2]]);
-    };
-
-    plugin.addEventListeners = function () {
-        plugin.log('addEventListeners');
-        plugin.$video.one('timeupdate', function () {
-            plugin.log('fire event "timeupdate" once');
-            plugin.getMiddleColor();
-            plugin.getColorPoll = setInterval(function () {
-                plugin.getMiddleColor();
-            }, options.timeUpdate);
+        this.$video.one('timeupdate', function () {
+            _this._publishColorData();
+            _this._setColorPoll();
         });
     };
 
-    plugin.init = function () {
-        plugin.canvas.width = CANVAS_WIDTH;
-        plugin.canvas.height = CANVAS_HEIGHT;
-        plugin.log('init:', CANVAS_WIDTH, CANVAS_HEIGHT);
-        plugin.addEventListeners();
+    /**
+     * @private
+     * Trigger event with color data
+     */
+    Plugin.prototype._publishColorData = function () {
+        var frameData = this._getMiddleColor();
+        this.$video.trigger('videohl-update', [frameData[0], frameData[1], frameData[2]])
     };
 
-    plugin.init.call(this);
+    /**
+     * @private
+     * Set a poll for getting color every N ms
+     */
+    Plugin.prototype._setColorPoll = function () {
+        var _this = this;
+
+        clearInterval(this.getColorPoll);
+
+        this.getColorPoll = setInterval(function () {
+            _this._publishColorData();
+        }, this.timeUpdate);
+    };
+
+    /**
+     * @public
+     * Set a time update
+     * @param {Number} [time=300]
+     */
+    Plugin.prototype.setTimeUpdate = function (time) {
+        this.timeUpdate = Number(time) || 300;
+        this._setColorPoll();
+    };
+
+    /**
+     * @public
+     * Return a time update
+     * @returns {Number}
+     */
+    Plugin.prototype.getTimeUpdate = function () {
+        return this.timeUpdate;
+    };
+
+    /**
+     * @public
+     * Destroy instance
+     */
+    Plugin.prototype.destroy = function () {
+        clearInterval(this.getColorPoll);
+        this.getColorPoll = null;
+        this.canvas = null;
+        this.$video.removeData('videohl-api');
+        this.$video.removeData('videohl-timeupdate');
+    };
+
+    this.each(function () {
+        var pluginApi,
+            $video = $(this);
+
+        pluginApi = new Plugin({
+            '$video': $video,
+            'timeUpdate': $video.data('videohl-timeupdate')
+        });
+
+        $video.data('videohl-api', pluginApi);
+    });
 
     return this;
 };
